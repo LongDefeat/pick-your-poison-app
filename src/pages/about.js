10db05347724db9852a708dc8302db40bd7c6cff
Devin Hunter <78935602+Devhunter16@users.noreply.github.com>
2023-03-10 16:19:54 -0700

    Update about.js
    Updated some styles and added <Head /> element to make styling consistent across pages.

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,13 +1,21 @@
 import React from "react";
+import Head from "next/head";
 import Navigation from "../components/routes-nav/Navigation";
 import styles from "../styles/About.module.css";
 
 function About() {
   return (
     <>
+      <Head>
+        <link
+          href="https://fonts.googleapis.com/css?family=Vibur:400"
+          rel="stylesheet"
+          type="text/css"
+        ></link>
+      </Head>
       <Navigation />
       <div className={styles.container}>
-        <h1 id={styles.h1}>About Us</h1>
+        <h1 id={styles.h1}>About our app</h1>
         <h3 id={styles.h3}>Welcome to our cocktail database!</h3>
         <p id={styles.p}>
           We started this website to encourage the art of cocktail making and
