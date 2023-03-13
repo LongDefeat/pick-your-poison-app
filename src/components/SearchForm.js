02b27744465e82e5a7033a7d4c179f12295f83b5
Devin Hunter <78935602+Devhunter16@users.noreply.github.com>
2023-03-12 17:28:16 -0700

    Add files via upload

---
+++ src/components/SearchForm.js
@@ -0,0 +1,31 @@
+import React from "react";
+import styles from "./styles/SearchForm.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faDice } from "@fortawesome/free-solid-svg-icons";
+
+export default function SearchForm(props) {
+  return (
+    <div id={styles.form}>
+        <form onSubmit={props.handleSubmit}>
+            <input
+            id={styles.input}
+            type="text"
+            value={props.searchTerm}
+            onChange={props.handleChange}
+            />
+            <button className={styles.btn} type="submit">
+                Search
+            </button>
+            <button
+            className={styles.btn}
+            onClick={
+                props.handleShowRandomCocktailRecipe
+            }
+            >
+                <FontAwesomeIcon id={styles.dice} icon={faDice} />
+                Random
+            </button>
+        </form>
+    </div>
+  );
+}
