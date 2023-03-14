c2de9a7d96fc09de3178ebb5968a653e55ed2ef9
Devin Hunter <78935602+Devhunter16@users.noreply.github.com>
2023-03-14 14:06:42 -0700

    Update RecipeDetails.js
    Destructured props here.

--- src/components/RecipeDetails.js
+++ src/components/RecipeDetails.js
@@ -1,17 +1,17 @@
 import React from "react";
 import styles from "./styles/RecipeDetails.module.css";
 
-export default function RecipeDetails(props) {
+export default function RecipeDetails({ name, image, recipe, ingredients }) {
   return (
     <>
       <div id={styles.body}>
-        <img className={styles.img} src={props.image}></img>
+        <img className={styles.img} src={image}></img>
         <div id={styles.description}>
-            <h1 id={styles.drinkTitle}>{props.name}</h1>
+            <h1 id={styles.drinkTitle}>{name}</h1>
             <h3 id={styles.ingredientsTitle}>Ingredients</h3>
             <ul>
               <div>
-                {props.ingredients.map((ingredient) => (
+                {ingredients.map((ingredient) => (
                   <li id={styles.ingredient}>
                     {ingredient}
                   </li>
@@ -19,7 +19,7 @@ export default function RecipeDetails(props) {
             </div>
           </ul>
           <h3 id={styles.directionsTitle}>Directions</h3>
-          <p id={styles.drinkDescription}>{props.recipe}</p>
+          <p id={styles.drinkDescription}>{recipe}</p>
         </div>
       </div>
     </>
