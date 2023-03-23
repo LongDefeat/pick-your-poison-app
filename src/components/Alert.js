1cbd84778c4c9ca8307eff224f48c348819907a9
Devin Hunter <78935602+Devhunter16@users.noreply.github.com>
2023-03-22 18:53:36 -0700

    Add files via upload

---
+++ src/components/Alert.js
@@ -0,0 +1,11 @@
+import React from "react";
+import styles from "./styles/Alert.module.css";
+
+export default function Alert(props) {
+  return (
+    <div id={styles.container}>
+        <h1>Whoops!</h1>
+        <p>Sorry! No cocktails found. Try another search!</p>
+    </div>
+  );
+}
