fff47b35b100485eb8b3162962210792480ceb79
Devin Hunter <78935602+Devhunter16@users.noreply.github.com>
2023-04-22 17:39:27 -0700

    Add files via upload
    Added new page so that each DrinksList has a unique URL and can use the "Home" link in the Navbar to go home.

---
+++ src/pages/drinks/[searchTerm].js
@@ -0,0 +1,120 @@
+import { useState, useEffect } from "react";
+import { useRouter } from 'next/router';
+import Head from "next/head";
+import searchCocktail from "../api/searchCocktail";
+import randomCocktail from "../api/randomCocktail";
+import { matchIngredientsWithMeasurements } from "../api/searchCocktail";
+import HomepageIntro from "../../components/HomepageIntro";
+import SearchForm from "../../components/SearchForm";
+import Navigation from "../../components/routes-nav/Navigation";
+import RecipeDetails from "../../components/RecipeDetails";
+import DrinksList from "../../components/DrinksList";
+import Alert from "../../components/Alert";
+
+export default function DrinksPage() {
+    const [parsedCocktails, setParsedCocktails] = useState([]);
+    const [parsedCocktail, setParsedCocktail] = useState(null);
+    const [showDrinkRecipe, setShowDrinkRecipe] = useState(false);
+    const [searchTerm, setSearchTerm] = useState("");
+    const [results, setResults] = useState([]);
+    const router = useRouter();
+
+    const {
+        query: {drinks}
+      } = router;
+    
+      // Set the parsedCocktail state when the component mounts
+    useEffect(() => {
+        if (drinks) {
+            console.log(drinks);
+            const parsedDrinks = JSON.parse(drinks);
+            setParsedCocktails(parsedDrinks);
+        }
+        else {
+            console.log("No drinks");
+        }
+    }, [drinks]);
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        // Prevents users from searching if the search field is empty
+        if (searchTerm.trim() === "") {
+            return;
+        }
+        setShowDrinkRecipe(false);
+        const data = await searchCocktail(searchTerm);
+        setResults(data);
+        setSearchTerm("");
+   
+        // Pushing variables through to page and setting the route
+        router.push({
+            pathname: `/drinks/${searchTerm}`,
+            query: { 
+                drinks: JSON.stringify(data)
+            }
+        })
+    };
+
+    const handleShowDrinkRecipe = (result) => {
+        setShowDrinkRecipe(true);
+        
+        // Pushing variables through to page and setting the route
+        router.push({
+            pathname: `/drink/${result.idDrink}`,
+            query: { 
+                drink: JSON.stringify(result)
+            }
+        })
+    };
+
+    const handleShowRandomCocktailRecipe = async () => {
+        const data = await randomCocktail();
+        const parsedCocktail = {
+          name: data.recipe.strDrink,
+          image: data.recipe.strDrinkThumb,
+          instructions: data.recipe.strInstructions,
+          ingredientsList: matchIngredientsWithMeasurements(data.recipe)
+        };
+        setParsedCocktail(parsedCocktail);
+        setShowDrinkRecipe(true);
+        setResults([]);
+        setSearchTerm("");
+    };
+
+    const handleChange = (e) => {
+        setSearchTerm(e.target.value);
+    };
+
+    return (
+        <div>
+          <Head>
+            <title> Nightcapp </title>{" "}
+            <link href="Home.module.css" rel="stylesheet" />
+            <link
+              href="https://fonts.googleapis.com/css2?family=League+Spartan&display=swap"
+              rel="stylesheet"
+            />
+            <link
+              href="https://fonts.googleapis.com/css?family=Vibur:400"
+              rel="stylesheet"
+              type="text/css"
+            ></link>{" "}
+          </Head>{" "}
+          <main>
+            <Navigation />
+            <HomepageIntro />
+            <SearchForm
+                handleSubmit={handleSubmit}
+                handleChange={handleChange}
+                handleShowRandomCocktailRecipe={handleShowRandomCocktailRecipe}
+                searchTerm={searchTerm}
+            />{" "}
+            <DrinksList
+                results={parsedCocktails}
+                handleShowDrinkRecipe={handleShowDrinkRecipe}
+            />
+            {!showDrinkRecipe && !results === true && <Alert />}
+          </main>{" "}
+        </div>
+    );
+}
