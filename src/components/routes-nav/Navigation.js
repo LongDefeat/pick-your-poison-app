4657bf3df4b03487877d450819fd0e878bffbd14
Devin Hunter <78935602+Devhunter16@users.noreply.github.com>
2023-04-22 19:43:12 -0700

    Update Navigation.js

--- src/components/routes-nav/Navigation.js
+++ src/components/routes-nav/Navigation.js
@@ -1,96 +1,86 @@
-import { useState, useContext } from "react";
-import { useRouter } from "next/router";
-import styles from "../styles/Home.module.css";
-import Head from "next/head";
-import searchCocktail from "./api/searchCocktail";
-import randomCocktail from "./api/randomCocktail";
-import CurrentUser from "../components/auth/CurrentUser";
-import Navigation from "../components/routes-nav/Navigation";
-import HomepageIntro from "../components/HomepageIntro";
-import SearchForm from "../components/SearchForm";
-import Alert from "../components/Alert";
+import React, { useContext } from "react";
+import Link from "next/Link";
+import styles from "../styles/Navigation.module.css";
+import UserContext from "../auth/CurrentUserContext";
 
-export default function Home() {
-  const [searchTerm, setSearchTerm] = useState("");
-  const [alert, setAlert] = useState(false);
-  const router = useRouter();
+function Navigation(props) {
+  // const { currentUser } = useContext(UserContext);
 
-  const currentUser = useContext(CurrentUser);
-
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-    // Prevents users from searching if the search field is empty
-    if (searchTerm.trim() === "") {
-      return;
-    }
-    const data = await searchCocktail(searchTerm);
-    setSearchTerm("");
-    if (data == undefined) {
-      setAlert(true);
-    }
-    else {
-      // Pushing variables through to page and setting the route
-      router.push({
-        pathname: `/drinks/${searchTerm}`,
-        query: { 
-          drinks: JSON.stringify(data)
-        }
-      })
-    }
-  };
-
-  const handleShowDrinkRecipe = (result) => {
-    // Pushing variables through to page and setting the route
-    router.push({
-        pathname: `/drink/${result.idDrink}`,
-        query: { 
-          drink: JSON.stringify(result)
-        }
-     })
-  };
-
-  const handleShowRandomCocktailRecipe = async () => {
-    const data = await randomCocktail();
-    handleShowDrinkRecipe(data.recipe);
-  };
-
-  const handleSetAlertFalse = () => {
-    setAlert(false);
+  function loggedInNav() {
+    return (
+      <>
+        <div className={styles.nav}>
+          <ul className={styles.navList}>
+            <li className={styles.navItem}>
+              <Link href="/">Home</Link>
+            </li>
+            <li className={styles.navItem}>
+              <Link href="/about" legacyBehavior>
+                About Us
+              </Link>
+            </li>
+            <li className={styles.navItem}>
+              <Link href="/">My Profile</Link>
+            </li>
+            <li className={styles.navItem}>
+              <Link href="/">Favorites</Link>
+            </li>
+            <li className={styles.navItem}>
+              <Link href="/">Log Out</Link>
+            </li>
+          </ul>
+        </div>
+      </>
+    );
   }
 
-  const handleChange = (e) => {
-    setSearchTerm(e.target.value);
-  };
+  function loggedOutNav() {
+    return (
+      <div className={styles.nav}>
+        <ul className={styles.navList}>
+          <li className={styles.navItem}>
+            <Link href="/">Home</Link>
+          </li>
+          <li className={styles.navItem}>
+            <Link href="/about" legacyBehavior>
+              About Us
+            </Link>
+          </li>
+          <li className={styles.navItem}>
+            <Link href="/">Log In</Link>
+          </li>
+          <li className={styles.navItem}>
+            <Link href="/">Sign Up</Link>
+          </li>
+        </ul>
+      </div>
+    );
+  }
 
   return (
-    <div>
-      <Head>
-        <title> Nightcapp </title>{" "}
-        <link href="Home.module.css" rel="stylesheet" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=League+Spartan&display=swap"
-          rel="stylesheet"
-        />
-        <link
-          href="https://fonts.googleapis.com/css?family=Vibur:400"
-          rel="stylesheet"
-          type="text/css"
-        ></link>{" "}
-      </Head>{" "}
-      <main className={styles.container}>
-        <Navigation 
-          currentUser={currentUser} 
-          handleSetAlertFalse={handleSetAlertFalse}
-        />
-        <HomepageIntro />
-        <SearchForm
-          handleSubmit={handleSubmit}
-          handleChange={handleChange}
-          handleShowRandomCocktailRecipe={handleShowRandomCocktailRecipe}
-          searchTerm={searchTerm}
-        />{" "}
-        {alert === true && <Alert />}
-      </main>{" "}
+    <div className={styles.nav}>
+      {/* {currentUser ? loggedInNav() : loggedOutNav()} */}
+
+      <ul className={styles.navList}>
+        <li className={styles.navItem}>
+          <Link href="/" onClick={props.handleSetAlertFalse}>
+            Home
+          </Link>
+        </li>
+        <li className={styles.navItem}>
+          <Link href="/about" legacyBehavior>
+            About Us
+          </Link>
+        </li>
+        {/**will need to DELETE THIS ONCE CURRENT USER WORKS */}
+        <li className={styles.navItem}>
+          <Link href="/signup" legacyBehavior>
+            Sign Up
+          </Link>
+        </li>
+      </ul>
     </div>
   );
 }
+
+export default Navigation;
