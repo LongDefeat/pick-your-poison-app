ea5f0904dcacb793002a21096797d7b3bc4a0770
Devin Hunter <78935602+Devhunter16@users.noreply.github.com>
2023-04-28 16:56:37 -0700

    Added headshots to the About page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,5 +1,8 @@
 import React from "react";
 import Head from "next/head";
+import Image from "next/image";
+import mason from "../../public/mason-Photoroom.png";
+import devin from "../../public/devin.jpg"
 import Navigation from "../components/routes-nav/Navigation";
 import styles from "../styles/About.module.css";
 
@@ -18,13 +21,29 @@ export default function About() {
       <Navigation />
       <div className={styles.container}>
         <h1 id={styles.h1}>About Nightcapp</h1>
-        <section id={styles.about}>         
-          <p id={styles.names}>
-            We're 
-            <span id={styles.mason}>Mason</span> 
-            and 
-            <span id={styles.devin}>Devin</span>.
-          </p>
+        <h2 id={styles.h2}>We're</h2>
+        <section id={styles.about}>     
+          <div className={styles.imageContainer}>
+            <div className={styles.masonContainer}>
+              <Image 
+                src={mason} 
+                alt="cocktail image" 
+                width={175} 
+                height={175}
+              />
+              <span id={styles.mason}>Mason</span>
+            </div>
+            <div id={styles.and}>&</div>
+            <div className={styles.devinContainer}>
+              <Image 
+                src={devin} 
+                alt="cocktail image" 
+                width={175} 
+                height={175}
+              />
+              <span id={styles.devin}>Devin</span>
+            </div>
+          </div>
           <p id={styles.p}>
             Welcome to Nightcapp, our cocktail database. Our passion for mixology inspired us to
             build this website and share our love of cocktails with others. Our web app
